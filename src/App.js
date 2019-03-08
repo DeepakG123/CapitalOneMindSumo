00895244e93bbdb921e1f1d57a1c786f2240fac9
Deepak Goel <goel.deepak9@gmail.com>
2019-03-08 01:08:42 -0500

    Added basic search bar that takes in a string and updates the query + rerenders the page on submit.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import ls from 'local-storage'
+
 
 const api_key = process.env.REACT_APP_API_KEY
 
 class App extends Component {
   state = {
       nasaData: "",
+      search: "Neptune"
   }
-  componentDidMount() {
 
-      axios.get("https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image")
+  handleUserInput = e => {
+    ls.set(e.target.id, e.target.value)
+  };
+
+  search = e => {
+    axios.get("https://images-api.nasa.gov/search?q=" + ls.get("search") + "&media_type=image")
+    .then((res => {
+    let data = res.data;
+    this.setState({nasaData: data.collection})
+    }))
+  }
+
+  componentDidMount() {
+      axios.get("https://images-api.nasa.gov/search?q=" + ls.get("search") + "&media_type=image")
       .then((res => {
       let data = res.data;
       this.setState({nasaData: data.collection})
@@ -19,20 +34,30 @@ class App extends Component {
 
   render() {
     if(this.state.nasaData != ""){
-        console.log(this.state.nasaData.items[39].data[0])
+        console.log(this.state)
     }
+    console.log(ls.get("search"))
     return (
       <div className="App">
+      <div className= "SearchForm">
+      <form>
+        <label>
+          Search for an Image:
+          <input  id = "search" type="text" name="name" onChange={e => this.handleUserInput(e)} />
+        </label>
+        <button onClick = {e => this.search(e)}> Submit </button>
+      </form>
+      </div>
       {(this.state.nasaData != "")
-      ?<header> {this.state.nasaData.items[39].data[0].title} </header>:
+      ?<header> {this.state.nasaData.items[20].data[0].title} </header>:
       <div> </div>
       }
       {(this.state.nasaData != "")
-      ?<img src = {this.state.nasaData.items[39].links[0].href}/>:
+      ?<img src = {this.state.nasaData.items[20].links[0].href}/>:
       <div> </div>
       }
       {(this.state.nasaData != "")
-      ?<p> {this.state.nasaData.items[39].data[0].description} </p>:
+      ?<p> {this.state.nasaData.items[20].data[0].description} </p>:
       <div> </div>
       }
       </div>

b136d524e68abc30c8551ffa43fceebd00170823
Deepak Goel <goel.deepak9@gmail.com>
2019-03-08 00:08:27 -0500

    Able to display photo from query

--- src/App.js
+++ src/App.js
@@ -6,20 +6,25 @@ const api_key = process.env.REACT_APP_API_KEY
 
 class App extends Component {
   state = {
-      nasaData: []
+      nasaData: "",
   }
   componentDidMount() {
-      axios.get('https://api.nasa.gov/EPIC/api/natural/images?api_key='+api_key)
+
+      axios.get("https://images-api.nasa.gov/search?q=apollo%2011&description=moon%20landing&media_type=image")
       .then((res => {
-      let nasaData = res.data;
-      this.setState({nasaData})
-      console.log(this.state.nasaData)
+      let data = res.data;
+      this.setState({nasaData: data.collection})
       }))
-  }
+}
 
   render() {
+    console.log(this.state.nasaData)
     return (
       <div className="App">
+      {(this.state.nasaData != "")
+      ?<img src = {this.state.nasaData.items[42].links[0].href}/>:
+      <div> </div>
+      }
       </div>
     );
   }
